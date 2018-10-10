1389994c0ed6f4ac08ca842d30c379cf7d887c20
socurites <socurites@wise-fashion.com>
2018-10-10 09:14:55 +0900

    Add chap10

---
+++ chap10/chap10.py
@@ -0,0 +1,58 @@
+"""
+Perceptron: scikit-laern
+"""
+
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron
+
+iris = load_iris()
+X = iris.data[:, (2, 3)]  # 꽃잎 길이, 꽃잎 너비
+y = (iris.target == 0).astype(np.int)
+
+per_clf = Perceptron(random_state=42)
+per_clf.fit(X, y)
+
+y_pred = per_clf.predict([[2, 0.5]])
+print(y_pred)
+
+
+"""
+MLP: Tensorflow high-level API: tf.estimator(>= 1.3) or tf.contrib.learn(< 1.3)
+"""
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+tf.logging.set_verbosity("INFO")
+
+mnist = input_data.read_data_sets("/tmp/data/")
+
+(X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+X_train = X_train.astype(np.float32).reshape(-1, 28*28) / 255.0
+X_test = X_test.astype(np.float32).reshape(-1, 28*28) / 255.0
+y_train = y_train.astype(np.int32)
+y_test = y_test.astype(np.int32)
+X_valid, X_train = X_train[:5000], X_train[5000:]
+y_valid, y_train = y_train[:5000], y_train[5000:]
+
+
+feature_cols = [tf.feature_column.numeric_column("X", shape=[28 * 28])]
+dnn_clf = tf.estimator.DNNClassifier(hidden_units=[300,100], n_classes=10,
+                                     feature_columns=feature_cols)
+
+# Train
+input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"X": X_train}, y=y_train, num_epochs=40, batch_size=50, shuffle=True)
+dnn_clf.train(input_fn=input_fn)
+
+# Eval
+test_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"X": X_test}, y=y_test, shuffle=False)
+eval_results = dnn_clf.evaluate(input_fn=test_input_fn)
+
+print(eval_results)
+
+
+# Predict
+y_pred_iter = dnn_clf.predict(input_fn=test_input_fn)
+y_pred = list(y_pred_iter)
+y_pred[0]
